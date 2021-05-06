0d95e0a5b13888535af4b11b698ea418370698b5
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-05-07 00:32:23 +0600

    Directory class created and modified. Fix bugs and Refactored some code

--- 14. File System and Directory/14.1 FileSystemAndFileDirectoryThird.php
+++ 14. File System and Directory/14.1 FileSystemAndFileDirectoryThird.php
@@ -6,12 +6,15 @@ class Dir{
 
     private $directories = [];
     private $files = [];
+    private $path;
+    private $directoryObject = [];
 
-    function __construct($currentDir){
-        $entries = scandir($currentDir);
+    function __construct($path){
+        $this->path = $path;
+        $entries = scandir($path);
         foreach($entries as $entry){
             if("." != $entry && ".." != $entry){
-                if(is_dir($entry)){
+                if(is_dir($path.DIRECTORY_SEPARATOR.$entry)){
                     array_push($this->directories, $entry);
                 }
                 else{
@@ -19,6 +22,23 @@ class Dir{
                 }
             }
         }
+    }
+
+    function getDir($index){
+        if(isset($this->directories[$index])) {
+            if (!isset($this->directoryObject[$index])) {
+                echo "creating new directory\n";
+                $this->directoryObject[$index] = new Dir($this->path . DIRECTORY_SEPARATOR . $this->directories[$index]);
+            }else{
+
+                echo "using old directory\n";
+            }
+            return $this->directoryObject[$index];
+        }
+        else{
+            throw new Exception("Directory doesn't exist!\n");
+        }
+        return false;
     }
 
     function getDirectory(){
@@ -34,3 +54,13 @@ $dir = new Dir(getcwd());
 
 print_r($dir->getDirectory());
 print_r($dir->getFiles());
+
+// created new directory
+$newdir = $dir->getDir(1);  // Tree view of directory :  Dir\
+print_r($newdir);               //                               ->Files\
+
+$anotherdir = $dir->getDir(0);  // created new directory
+print_r($anotherdir->getFiles());
+
+$anotherdir = $dir->getDir(0);  // using old directory/object
+print_r($anotherdir->getFiles());
