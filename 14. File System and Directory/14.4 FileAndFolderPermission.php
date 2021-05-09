d076ad94f8646792edfb4d7b25e8cd11e2ea8517
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-05-10 01:31:12 +0600

    File and folder permissions

---
+++ 14. File System and Directory/14.4 FileAndFolderPermission.php
@@ -0,0 +1,15 @@
+<?php
+
+// File and folder permission
+
+/*  0777 = octal value. It means all can read , write and execute
+
+ *  Unix Permissions Calculator
+ *  User                Group               Public
+ * ------               -----               ------
+ * Read = 0400          Read = 0040         Read = 0004
+ * Write = 0200         Write = 0020        Write = 0002
+ * Execute = 0100       Execute = 0010      Execute = 0001
+ *
+ * to know more : http://permissions-calculator.org/
+ * */
