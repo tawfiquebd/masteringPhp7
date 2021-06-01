55507b54a5c54af511edc19a56ac5f274db6d85e
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-06-01 23:01:54 +0600

    php error log showing in our custom file

---
+++ 17. Error/17.2 errorLogInFile.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set("display_errors",0);    //
+ini_set("display_startup_errors",0);
+
+// php error log show in our custom file path :  c:\php\php_error.txt
+// go to php.ini file and edit this file .
+// comment out this line  ->  error_log = c:\php\php_error.txt      // creat a file in php folder
+
+//echo phpinfo();
+
+echo $ok;
