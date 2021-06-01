816a7dc089cd01ae586cc256ff0b45bb1d43dd44
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-06-01 20:07:00 +0600

    php error handling basic

---
+++ 17. Error/17.1 error.php
@@ -0,0 +1,20 @@
+<?php
+
+// PHP Error Handling
+
+//error_reporting(0); // Error showing off.
+//error_reporting(E_NOTICE); // Only notice type error will show
+//error_reporting(E_WARNING); // Only warning type error will show
+
+//error_reporting(E_ALL);  // Show all error type
+
+//error_reporting(E_NOTICE | E_WARNING);  // multiple error show
+
+//echo $result;   // it will show warning
+
+//echo get_cfg_var("cfg_file_path");    // to check which configuration file loaded
+// or go to terminal and type : php --ini   // to see which ini file loaded for php
+
+//ini_set("display_errors",0);  // set php.ini file, Edit machine php ini file display_errors = Off
+
+echo $result;
