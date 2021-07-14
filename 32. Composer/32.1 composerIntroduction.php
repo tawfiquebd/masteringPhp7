f18234a1bd6317436d0d1a419132ab845b437db9
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2021-07-14 23:57:34 +0600

    package install, version update and remove package

--- 32. Composer/32.1 composerIntroduction.php
+++ 32. Composer/32.1 composerIntroduction.php
@@ -4,10 +4,32 @@
 // make a directory/folder. => command in cmd/terminal : mkdir directory/folder name
 
 /*
-* To install a composer
+* To install a Package
  * --------------------
  * Make a directory/folder first in your project root dir
  * then go to packagist.org  to get the package name . Ex: ramsey/uuid
  * then goto that directory where you want to install the package
  * And now in the terminal type : composer require ramsey/uuid
 */
+
+// To install a package with selected/older version
+/**
+ * composer require packageName:version
+ * Ex: composer require paragonie/sodium_compat:1.15.0
+ */
+
+
+// To update a installed package
+/**
+ * Go to package directory folder and open composer.json file
+ * Change the version FROM "paragonie/sodium_compat": "1.15.1"  TO => "paragonie/sodium_compat": "1.16.1"
+ * Then open terminal and type : composer update
+ * It will update the package......
+ */
+
+// To remove a package from project
+/**
+ * Go to that project folder and open terminal
+ * type => composer remove packageName
+ * Ex: composer remove paragonie/sodium_compat
+ */
---
+++ 32. Composer/sodium_compat/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '74cf3eb8853967abff72bb68286eccd0845edef7',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '74cf3eb8853967abff72bb68286eccd0845edef7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
